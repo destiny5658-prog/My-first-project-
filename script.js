6150cb817dbbea802153c20374a91be451540c25
destiny5658-prog <destinyose5658@gmail.com>
2025-08-22 17:53:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function appendToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function operator(op) {
+  document.getElementById('display').value += op;
+}
+
+function deleteLast() {
+  const display = document.getElementById('display');
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  const display = document.getElementById('display');
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
